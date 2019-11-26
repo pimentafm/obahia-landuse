6ade332a3b232e4698469e807d4f0dd663af9608
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2019-11-26 18:18:28 -0300

    Change the footer z-index to 1.

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const FooterContainer = styled.footer`
     background: #fff;
     position: fixed;
-    padding: 10px;
+    margin: 1px 1px 1px 1px;
+    padding: 5px;
     display: flex;
     flex-direction: row;
     box-shadow: 0px 2px 3px rgba(0,0,0,.13) ,1px 2px 2px rgba(0,0,0,.1) , -1px -2px 2px rgba(0,0,0,.05) ;
@@ -11,6 +12,7 @@ export const FooterContainer = styled.footer`
     bottom: 0;
     width: 100%;
     text-align: center;
+    z-index: 1;
 
     #select {
         width: 100px;
