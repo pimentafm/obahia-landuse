88a241eea3ac568f2462eee6fdf19f228ca0ed0c
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-02-17 17:34:50 -0300

    Add drainage select and functions in menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 import { Select, Icon } from "antd";
+
+import oba from "../../services/api";
 
 import { MenuContainer } from "./styles";
 import LayerSwitcher from "../../components/LayerSwitcher";
@@ -14,6 +16,7 @@ const { Option } = Select;
 const Menu = props => {
   const [defaultYear] = useState(props.defaultYear);
   const [defaultWatershed] = useState(props.defaultWatershed);
+  const [defaultCodeName] = useState(props.defaultCodeName);
   const [defaultCategory, setCategory] = useState(props.defaultCategory);
   const [categories] = useState([
     ["Região", "/"],
@@ -24,8 +27,31 @@ const Menu = props => {
     Array.from(new Array(29), (val, index) => index + 1990)
   );
   const [watersheds] = useState(["grande", "corrente", "carinhanha"]);
+  const [codenames, setCodenames] = useState([]);
   const [isHidden, setHidden] = useState(props.isHidden);
   const [map] = useState(props.map);
+
+  useEffect(() => {
+    oba.post("geom/", {
+      table_name: "drainageareas",
+      headers: {
+        "Content-type": "application/json"
+      }
+    })
+    .then(response => {
+      const data = response.data.map(({ name, code }) => ({ name, code }));
+      
+      const names = data.map(n => n.name);
+      const codes = data.map(n => n.code);
+
+      const codenames = names.map((n, c) => n + ' - '+ codes[c]);
+
+      setCodenames(codenames);
+    })
+    .catch(e => {
+      this.errors.push(e);
+    });
+  },[props.defaultCategory]);
 
   const handleCategory = e => {
     setCategory(e);
@@ -67,6 +93,28 @@ const Menu = props => {
     watershedSelect = null;
   }
 
+  let drainageLabel = null;
+  let drainageSelect = null;
+
+  if (defaultCategory === "Área de drenagem") {
+    drainageLabel = <label>Nome</label>;
+    drainageSelect = (
+      <Select
+        id="select"
+        defaultValue={defaultCodeName}
+        onChange={props.handleDrainage}
+      >
+        {codenames.map(c => (
+          <Option key={c} value={c}>
+            {c}
+          </Option>
+        ))}
+      </Select>
+    );
+  } else {
+    drainageSelect = null;
+  }
+
   return (
     <MenuContainer isHidden={isHidden}>
       <div id="nav" className="nav">
@@ -96,6 +144,9 @@ const Menu = props => {
 
       {watershedsLabel}
       {watershedSelect}
+
+      {drainageLabel}
+      {drainageSelect}
 
       <label>Ano</label>
       <Select
