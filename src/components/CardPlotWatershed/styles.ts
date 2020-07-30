338281c5245521423da156007fa4ddc8f681f26d
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-07-30 16:11:02 -0300

    Adding mobile media query in all operation levels

--- src/components/CardPlotWatershed/styles.ts
+++ src/components/CardPlotWatershed/styles.ts
@@ -19,6 +19,12 @@ export const Container = styled.div<ContainerProps>`
   transform: translateX(${({ ishidden }) => (ishidden ? 30 : 0)}vw);
   transition: 0.3s;
 
+  @media only screen and (max-width: 760px) {
+    width: 80vw;
+    transform: translateX(${({ ishidden }) => (ishidden ? 80 : 0)}vw);
+    transition: 0.3s;
+  }
+
   label {
     margin-top: 5px;
 
