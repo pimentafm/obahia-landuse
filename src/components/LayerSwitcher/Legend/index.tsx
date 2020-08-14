78fbc22da62c3fb949d52cba4cfa3279562482cd
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-08-14 16:48:14 -0300

    Translating legend

--- src/components/LayerSwitcher/Legend/index.tsx
+++ src/components/LayerSwitcher/Legend/index.tsx
@@ -5,12 +5,16 @@ import { wms } from '../../../services';
 
 import { Container } from './styles';
 
+import { useTranslation } from 'react-i18next';
+
 interface LegendProps {
   name: string;
   isvisible: boolean;
 }
 
 const Legend: React.FC<LegendProps> = ({ name, isvisible }) => {
+  const { t } = useTranslation();
+  
   const [legendHTML, setlegendHTML] = useState([]);
 
   useEffect(() => {
@@ -21,11 +25,21 @@ const Legend: React.FC<LegendProps> = ({ name, isvisible }) => {
       .then(res => {
         let html = res.data;
 
+        html = html.replace('Formações florestais', t('label_forest'))
+                   .replace('Formações savânicas', t('label_savanna'))
+                   .replace('Formações campestres', t('label_grasslands'))
+                   .replace('Mosaico de agricultura ou pastagem', t('label_mosaic'))
+                   .replace('Agricultura de sequeiro', t('label_rainfed'))
+                   .replace('Agricultura irrigada', t('label_irrigated'))
+                   .replace('Pastagem', t('label_pasture'))
+                   .replace(`Corpos d'água`, t('label_water'))
+                   .replace('Área urbana/Construções rurais', t('label_urban'));
+
         html = ReactHtmlParser(html);
 
         setlegendHTML(html);
       });
-  }, [name]);
+  }, [name, t]);
   return (
     <Container id="layerswitcher" isvisible={isvisible}>
       {legendHTML}
