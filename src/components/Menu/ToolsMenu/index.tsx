4faeeef1d4979a6d27cb0f28ad1efd4285f42721
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-10-05 13:21:45 -0300

    Adding the icon of the web map server on tools menu

--- src/components/Menu/ToolsMenu/index.tsx
+++ src/components/Menu/ToolsMenu/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 
-import { GiMeshBall, GiStack, GiRaining } from 'react-icons/gi';
+import {
+  GiStack,
+  GiRaining,
+  GiMeshBall,
+  GiEarthAmerica,
+  // GiMoneyStack,
+  // GiShakingHands,
+} from 'react-icons/gi';
 import { MdTrendingDown } from 'react-icons/md';
 
 import { Popover } from 'antd';
@@ -18,6 +25,16 @@ const ToolsMenu: React.FC<ToolsMenuProps> = ({ ishidden }) => {
 
   return (
     <Container ishidden={ishidden}>
+      <Popover placement="right" content={t('toolsmenu_mapserver')}>
+        <GiEarthAmerica
+          className="text-icon"
+          style={{ fontSize: 25, color: '#AAD3DF', cursor: 'pointer' }}
+          onClick={() =>
+            window.open('http://obahia.dea.ufv.br/maps/38/view', '_self')
+          }
+        />
+      </Popover>
+
       <Popover
         placement="right"
         title={t('toolsmenu_maps')}
